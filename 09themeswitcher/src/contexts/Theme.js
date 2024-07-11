921c78d405317a4a7a1d7512c06bbfef20083c8d
Khushichauhan <kc6478576@gmail.com>
2024-07-11 15:09:11 +0530

    Add Todo Projec

--- 09themeswitcher/src/contexts/Theme.js
+++ 09themeswitcher/src/contexts/Theme.js
@@ -1,8 +1,8 @@
 import  {createContext, useContext} from "react";
 
 export const ThemeContext = createContext({
-    themeMode: "light",
-    darkTheme: () => {}, 
+    themeMode: "light",  //String Expected
+    darkTheme: () => {},  //functionality nahi likhi jaati bass bata diya jaata hai ki aap ek function ho ishki functoinality App.jsx me likhte hai
     lightTheme: () => {},
 })
 
