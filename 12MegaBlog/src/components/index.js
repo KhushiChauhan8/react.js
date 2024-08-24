b030d19477063bbe81e8f86cd237b880b407911d
Khushichauhan <kc6478576@gmail.com>
2024-08-24 09:49:02 +0530

    Mega Projecr Add

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,10 +1,17 @@
+import Select from './Select'
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/Container"
-import Logo from "./Logo"
-import LogoutBtn from "./Header/LogoutBtn"
-import Button from './Button'
-import Input from './Input'
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from './Button';
+import Input from './Input';
+import RTE from './RTE';
+import Signup from './Signup';
+import Login from './Login';
+import PostForm from './post-form/PostForm'
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
 
 export {
     Header,
@@ -13,5 +20,13 @@ export {
     Logo,
     LogoutBtn,
     Button,
-    Input
+    Input,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Select
+    
 }
